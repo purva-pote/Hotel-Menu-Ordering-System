a960ae8f4e62143afd4f29460beb788dc260eb3a
Aarohi Paranjape <aarohiparanjape3@gmail.com>
2025-10-24 01:27:48 +0530

    created feedback page

--- user/feedback_win.java
+++ user/feedback_win.java
@@ -1,5 +1,28 @@
 package user;
+import javax.swing.*;
 
 public class feedback_win {
-    
+    public void show() {
+        JFrame win = new JFrame("Feedback");
+        win.setSize(300, 200);
+        JLabel lb = new JLabel("Feedback or tip?");
+        lb.setBounds(30,40,120,25);
+        win.add(lb);
+        JTextField txt = new JTextField();
+        txt.setBounds(30,70,200,25);
+        win.add(txt);
+        JButton b = new JButton("Submit");
+        b.setBounds(30,110,100,25);
+        win.add(b);
+        b.addActionListener(e->{
+            try(PrintWriter w=new PrintWriter(new FileWriter("feedback.txt",true))){
+                w.println(txt.getText());
+            }catch(Exception x){}
+            JOptionPane.showMessageDialog(win,"Thank you!");
+            win.dispose();
+            System.exit(0);
+        });
+        win.setLayout(null);
+        win.setVisible(true);
+    }
 }
